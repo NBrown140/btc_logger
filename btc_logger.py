eeae37e89551e151fbcdb076ec002179db3e00ac
Nicolas Brown <nic.brown0@gmail.com>
2017-07-05 18:41:19 -0700

    Add files via upload

---
+++ btc_logger.py
@@ -0,0 +1,19 @@
+from bitstampy import api
+import csv, time
+
+path_data = r"D:\Bitcoin\bitcoin_prices.csv"
+path_orders = r"D:\Bitcoin\bitcoin_orders.csv"
+
+while True:
+	# Get data from bitstamp
+	ticker = api.ticker()
+
+	# Log data to file
+	row = [ticker['timestamp'], ticker['volume'], ticker['last'], ticker['high'], ticker['low'], ticker['bid'], ticker['ask']]    
+	print row
+	with open(path_data, 'ab') as f:
+	    w = csv.writer(f)
+	    w.writerow(row)
+	
+	# Wait 60 seconds
+	time.sleep(60)
