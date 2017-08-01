612fc6ddbdb9cfa49ecefd7ae2daf2dcbd06f20e
Nicolas Brown <nic.brown0@gmail.com>
2017-08-01 16:57:25 -0400

    Update tweet_logger.py

--- tweet_logger.py
+++ tweet_logger.py
@@ -1,7 +1,7 @@
 """ Inspired from: https://github.com/llSourcell/twitter_sentiment_challenge """
 
-import tweepy
-import numpy
+import tweepy, csv
+import numpy as np
 from textblob import TextBlob
 from bitstampy import api
 
@@ -21,13 +21,13 @@ access_token_secret='ACCESS_TOKEN_SECRET_HERE'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api_tweet = tweepy.API(auth)
 
 
 # Retrieve Tweets
-search_results = [api.search(q=tag, count=tweet_count) for tag in tags]
+search_results = [api_tweet.search(q=tag, count=tweet_count) for tag in tags]
 
-for serch_result,tag in zip(search_results, tags):
+for search_result,tag in zip(search_results, tags):
     pol_sub_array = np.empty((tweet_count,2))   #Polarity and subjectivity array
     count = 0
     for tweet in search_result:
@@ -41,9 +41,10 @@ for serch_result,tag in zip(search_results, tags):
     median = np.median(pol_sub_array, axis=0)
     std = np.std(pol_sub_array, axis=0)
     # Make row
+    ticker = api.ticker()
     row = [ticker['timestamp'], avg[0], median[0], std[0], avg[1], median[1], std[1]]
     # Save statistics
-    specific_path = root_path.split('.')[0]+'_'+tag+root_path.split('.')[1]
+    specific_path = root_path.split('.')[0]+'_'+tag+'.'+root_path.split('.')[1]
     with open(specific_path, 'ab') as f:
         w = csv.writer(f)
-    w.writerow(row)
+        w.writerow(row)
