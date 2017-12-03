0e3258f3cb571fb6f5aa51237935c46fb92cf425
Nicolas <nic.brown0@gmail.com>
2017-12-02 22:04:10 -0500

    switched from Bitstampy bitstamp-python-client

--- btc_logger.py
+++ btc_logger.py
@@ -1,15 +1,25 @@
-from bitstampy import api
-import csv
+import bitstamp.client
+import csv, os
 
-path_data = r"D:\Bitcoin\bitcoin_prices.csv"
+base_path = r"D:\Bitcoin"
 
 # Get data from bitstamp
-ticker = api.ticker()
+public_client = bitstamp.client.Public()
 
-# Log data to file
-row = [ticker['timestamp'], ticker['volume'], ticker['last'], ticker['high'], ticker['low'], ticker['bid'], ticker['ask']]    
-print row
-with open(path_data, 'ab') as f:
-    w = csv.writer(f)
-    w.writerow(row)
+currencies_list = ['btc', 'eur', 'xrp', 'ltc', 'eth']
+
+for currency in currencies_list:
+	# Log data to file
+	row = [public_client.ticker(base=currency, quote="usd")['timestamp'], \
+	public_client.ticker(base=currency, quote="usd")['volume'], \
+	public_client.ticker(base=currency, quote="usd")['last'], \
+	public_client.ticker(base=currency, quote="usd")['high'], \
+	public_client.ticker(base=currency, quote="usd")['low'], \
+	public_client.ticker(base=currency, quote="usd")['bid'], \
+	public_client.ticker(base=currency, quote="usd")['ask']]    
+	print row
+	path_data = os.path.join(base_path, "bitstamp_"+currency+"_usd.csv")
+	with open(path_data, 'ab') as f:
+	    w = csv.writer(f)
+	    w.writerow(row)
 
